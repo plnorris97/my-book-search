56cd477799b8875ce5743462363f87f4c27ecbca
Tricia Norris <tricia@Tricias-MacBook-Air.local>
2019-01-08 12:01:35 -0500

    components and pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Search from './pages/Search';
+import Library from './pages/Library';
+import Detail from "./pages/Detail";
+import NoMatch from "./pages/NoMatch";
 import AppNavbar from './components/AppNavbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
+function App() {
+  return (
+    <Router>
       <div>
-       <AppNavbar />
+        <AppNavbar />
+        <Switch>
+          <Route exact path="/" component={Search} />
+          <Route exact path="/books" component={Search} />
+          <Route exact path="/books/:id" component={Detail} />
+          <Route exact path="/books/library" component={Library} />
+          <Route component={NoMatch} />
+        </Switch>
       </div>
-    );
-  }
+    </Router>
+  );
 }
 
 export default App;
---
+++ client/src/pages/Search.js
@@ -0,0 +1,94 @@
+import React, { Component } from "react";
+// import DeleteBtn from "../components/DeleteBtn";
+import Jumbotron from "../components/Jumbotron";
+import API from "../utils/API";
+import { Link } from "react-router-dom";
+import { Col, Row, Container } from "../components/Grid";
+// import { List, ListItem } from "../components/List";
+import { Input, FormBtn } from "../components/Form";
+import Card from '../components/Card'
+
+class Search extends Component {
+  state = {
+    books: [],
+    title: "",
+    author: "",
+    description: ""
+  };
+
+  componentDidMount() {
+    this.loadBooks();
+  }
+
+  loadBooks = () => {
+    API.getBooks()
+      .then(res =>
+        this.setState({ books: res.data, title: "", author: "", description: "" })
+      )
+      .catch(err => console.log(err));
+  };
+
+  deleteBook = id => {
+    API.deleteBook(id)
+      .then(res => this.loadBooks())
+      .catch(err => console.log(err));
+  };
+
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    if (this.state.title && this.state.author) {
+      API.saveBook({
+        title: this.state.title,
+        author: this.state.author,
+        synopsis: this.state.synopsis
+      })
+        .then(res => this.loadBooks())
+        .catch(err => console.log(err));
+    }
+  };
+
+  render() {
+    return (
+      <Container fluid>
+        <Row>
+          <Col size="md-12">
+            <Jumbotron>
+              <h1>My Book Search</h1>
+            </Jumbotron>
+            <form>
+              <Input
+                value={this.state.title}
+                onChange={this.handleInputChange}
+                name="title"
+                placeholder="Title (required)"
+              />
+              <FormBtn
+                disabled={!(this.state.author && this.state.title)}
+                onClick={this.handleFormSubmit}
+              >
+                Search
+              </FormBtn>
+            </form>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Card>
+              {/* Results go here. */}
+            </Card>
+          
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+export default Search;
