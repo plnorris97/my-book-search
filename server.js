c2bc6fe9fc1fce5bd37e33857ea30a19c4588f2e
Tricia Norris <tricia@Tricias-Air.richmond.edu>
2019-01-12 11:33:52 -0500

    server again

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+    res.sendFile(path.join(__dirname, "./client/public/index.html"));
 });
 
 //Bodyparser middleware
