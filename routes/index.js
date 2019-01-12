f0bf715547e67df8711abbb30dd227c3459ca7b0
Tricia Norris <tricia@Tricias-Air.richmond.edu>
2019-01-12 11:37:37 -0500

    server yet again

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ router.use("/api", apiroutes);
 
 // What is this router.use for?
 router.use((req, res) =>{
-  res.sendFile(path.join(__dirname, "../client/build/index.html"))
+  res.sendFile(path.join(__dirname, "../client/public/index.html"))
 })
 
 module.exports = router;
