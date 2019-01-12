a0ae28e29aa95894d77fab152ccaa3214e4a8a65
Tricia Norris <tricia@Tricias-Air.richmond.edu>
2019-01-12 13:42:21 -0500

    server and ports

--- config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    mongoURI: "mongodb://plnorris97:December192015!@ds151354.mlab.com:51354/books-search"
-
-}
--- routes/api/books.js
+++ routes/api/books.js
@@ -1,36 +1,47 @@
-const express = require('express');
-const router = express.Router();
+const router = require("express").Router();
+const booksController = require("../../controller/dbController");
 
-const Books = require('../../models/books')
 
 // @route   GET api/books
 // @desc    Get all books
 // @access  Public
 
-router.get("/", (req, res) => {
-  Books.findAll()
-    .then(books => res.json(books))
-})
+// router.get("/", (req, res) => {
+//   Books.findAll()
+//     .then(books => res.json(books))
+// })
+
+// Matches with "/api/books"
+router.route("/")
+  .get(booksController.findAll)
+  .post(booksController.create);
 
 // @route   POST api/books
 // @desc    Create a book and save to database
 // @access  Public
 
-router.post("/", (req, res) => {
-  const newBook = new Book({
-  });
-  newBook.save().then(book => res.json(book));
-})
+// router.post("/", (req, res) => {
+//   const newBook = new Book({
+//   });
+//   newBook.save().then(book => res.json(book));
+// })
+// Matches with "/api/books/:id"
+router
+  .route("/:id")
+  .get(booksController.findById)
+  .put(booksController.update)
+  .delete(booksController.delete);
+
 
 // @route   DELETE api/books/:id
 // @desc    Delete a book from the database
 // @access  Public
 
-router.delete("/:id", (req, res) => {
-  Books.findById(req.params.id).then(book => book.delete()
-  .then(() => res.json({ success: true}))
-  .catch(err => res.status(404).json({ success: false})))
-})
+// router.delete("/:id", (req, res) => {
+//   Books.findById(req.params.id).then(book => book.delete()
+//   .then(() => res.json({ success: true}))
+//   .catch(err => res.status(404).json({ success: false})))
+// })
 
 
 //   .get(dbController.findAll)
