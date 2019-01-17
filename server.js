bd1e26b1c31fbda950edcde0f837d34167c38848
Tricia Norris <tricianorris97@gmail.com>
2019-01-17 13:37:52 -0500

    ready to deploy

--- server.js
+++ server.js
@@ -4,16 +4,14 @@ const bodyParser = require('body-parser');
 const routes = require("./routes");
 const PORT = process.env.PORT || 3001;
 const app = express();
+// const secret = process.env.SECRET || "some secret passphrase here for local development"
+const path = require('path');
 
 //Bodyparser middleware
 app.use(bodyParser.json());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
-}
 
 app.use(routes)
 
@@ -26,5 +24,12 @@ mongoose
     .then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err));
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+}
+    
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
